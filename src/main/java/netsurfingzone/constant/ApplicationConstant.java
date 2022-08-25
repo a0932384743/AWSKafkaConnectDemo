6682f64db312b3285bbf586ba3c025d4ba9dba0b
jr_wei_chen <a09323843743@gmail.com>
2022-08-25 10:11:24 +0800

    first commit

---
+++ src/main/java/netsurfingzone/constant/ApplicationConstant.java
@@ -0,0 +1,9 @@
+package netsurfingzone.constant;
+
+public class ApplicationConstant {
+	public static final String KAFKA_LOCAL_SERVER_CONFIG = " b-1.sksmskcluster1.nwod1j.c4.kafka.ap-northeast-1.amazonaws.com:9092,b-2.sksmskcluster1.nwod1j.c4.kafka.ap-northeast-1.amazonaws.com:9092,b-3.sksmskcluster1.nwod1j.c4.kafka.ap-northeast-1.amazonaws.com:9092";
+	public static final String GROUP_ID_STRING = "group-id-string-1";
+	public static final String TOPIC_NAME = "netsurfingzone-topic-1";
+	public static final String KAFKA_LISTENER_CONTAINER_FACTORY = "kafkaListenerContainerFactory";
+	public static final String GROUP_ID_JSON = "group-id-json-1";
+}
